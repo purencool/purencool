e7f848cc5a7eca34f917c622021c3b8a6850f635
purencool <purencool@gmail.com>
2018-02-12 16:55:36 +1100

    Added video modules so iframe will be inserted properly

--- react/src/js/components/widgets/ContentVideo.js
+++ react/src/js/components/widgets/ContentVideo.js
@@ -1,4 +1,6 @@
 import React from "react";
+import ReactDOM from "react-dom";
+import YoutubeEmbedVideo from "youtube-embed-video";
 
 /**
  *  This component is a template to display 
@@ -9,8 +11,7 @@ export default class ContentVideo extends React.Component {
     const { video } = this.props;
     return (
       <div class="col-md-6">
-        <iframe width="100%"   src="{video}"  allow="autoplay; encrypted-media" >
-         </iframe>
+        <YoutubeEmbedVideo videoId={video.split( '=' )[1]} suggestions={false} />
       </div>
     );
   }
